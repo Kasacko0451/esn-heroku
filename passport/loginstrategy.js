ec0f454800144514bdd93e583430296281559ab8
Kasacko0451 <ivan.bakotin1999@gmail.com>
2021-08-04 21:06:49 +0200

    Update loginstrategy.js

--- passport/loginstrategy.js
+++ passport/loginstrategy.js
@@ -2,17 +2,17 @@ const LocalStrategy = require("passport-local").Strategy;
 const pool = require("../db.js");
 
 const loginStrategy = new LocalStrategy( async (username, password, done) => {
+    
     pool.connect()
-
+    
     const res_user = await pool.query("SELECT * FROM users WHERE username=$1", [username])
-    console.log(res_user.rows, "heloooooooooooo")
+    
     if (!res_user.rows[0]) return done("Username or password not valid", null);
 
     if (password !== res_user.rows[0].password) return done("Username or password not valid", null);
  
-    console.log(res_user.rows, "heloooooooooooo")
     return done(null, res_user.rows[0].username)
-    pool.end()
+
 });
 
 module.exports = loginStrategy;
