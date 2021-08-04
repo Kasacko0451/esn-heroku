2576a8fda4d14d9efdc9404afafa0028381f0925
Kasacko0451 <ivan.bakotin1999@gmail.com>
2021-08-04 13:25:08 +0200

    Add files via upload

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "g2esportslec12",
-  host: "localhost",
-  port: 5432,
-  database: "esnproto",
+  user: "postgres" || process.env.user,
+  password: "g2esportslec12" || process.env.password,
+  host: "localhost" || process.env.host,
+  port: 5432 || process.env.port,
+  database: "esnproto" || process.env.database,
 });
 
 module.exports = pool;
