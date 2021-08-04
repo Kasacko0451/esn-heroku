6ccecbe2ef77d88eedd41fffe72046b50276680a
Kasacko0451 <ivan.bakotin1999@gmail.com>
2021-08-04 21:05:01 +0200

    Update index.js

--- passport/index.js
+++ passport/index.js
@@ -7,14 +7,13 @@ passport.serializeUser(function(user, done) {
   });
   
 passport.deserializeUser(function(username, done) {
-    console.log(username)
-    pool.connect()
+    
     pool.query("SELECT * FROM users WHERE username=$1", [username], function(err, result) {
-        console.log("loginindex", result.rows)
-        pool.end()
-        console.log("2222222222", result.rows)
+
         done(err, result.rows[0]) 
-        console.log("3333333333", result.rows)
+        
+        pool.end()
+
     })
 });
 
