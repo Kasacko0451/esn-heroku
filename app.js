68086004d40610a1d03dc3d16868f0e78a26bcf4
Kasacko0451 <ivan.bakotin1999@gmail.com>
2021-09-21 15:48:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const all_routes = require("./routes/all_routes.js");
 const pool = require("./db.js");
 const PORT = process.env.PORT || 8080;
 
+pool.connect()
+
 const pgSession = require('connect-pg-simple')(session)
 
 const authCheck = (req, res, next) => {
